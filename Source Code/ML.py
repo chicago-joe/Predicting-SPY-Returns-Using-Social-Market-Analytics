feb0d6b4752b327e9b6a6277cc6be1b59cc66b46
zenithzhou <zzhou64@illinois.edu>
2019-10-20 22:10:35 -0500

    machine learning file ad

---
+++ Source Code/ML.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 21:54:04 2019
+
+@author: Zenith Zhou
+
+"""
+
+import seaborn as sns
+import pandas as pd
+import time
+import numpy as np
+
+df = pd.read_csv("D:\Github Repo\SMA-HullTrading-Practicum\
+                 Source Code\Data\SPYstationarity.txt",delimiter = ",")
+
